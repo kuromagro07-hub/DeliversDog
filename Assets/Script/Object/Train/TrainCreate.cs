a0559c9170c9ed1f410e0360afbfa4f840a1bb56
kuromagro07-hub <kuromagro07@gmail.com>
2025-10-29 23:31:57 +0900

    add: プレイヤーのモデル、アニメーション fix: 電車とレールの生成位置とサイズの微調整、プレイヤーのアニメーションとモデル反映

--- Assets/Script/Object/Train/TrainCreate.cs
+++ Assets/Script/Object/Train/TrainCreate.cs
@@ -1,9 +1,12 @@
+// 生成位置を制御したい場合は、コントローラークラスで調整する
+
 using UnityEngine;
 using System.Collections;
 
 public class TrainCreate : MonoBehaviour
 {
     [SerializeField] GameObject trainPrefab;
+    Vector3 spawnPos = new Vector3(14.5f, -5.0f, -250f);
 
     private void Start()
     {
@@ -12,7 +15,7 @@ public class TrainCreate : MonoBehaviour
 
     void Create()
     {
-        Instantiate(trainPrefab);
+        Instantiate(trainPrefab, spawnPos, Quaternion.identity);
     }
 
     // コルーチン本体
